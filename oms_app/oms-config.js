5a55ad4713454973c16fd52090fe5fd55cc10b3a
shilei <goodfriendshilei@163.com>
2026-07-04 04:35:44 +0800

    Route Feishu identity exchange to OMS bridge

--- oms_app/oms-config.js
+++ oms_app/oms-config.js
@@ -1,5 +1,5 @@
 window.OMS_FEISHU_APP_ID = "cli_aaac7e6da2b95cfc";
 window.OMS_FEISHU_REDIRECT_URI = "https://ponslucia14-ux.github.io/huangjia-oms-v1/";
 window.OMS_FEISHU_SCOPE_LIST = [];
-window.OMS_AUTH_ENDPOINT = window.OMS_AUTH_ENDPOINT || "/api/feishu/identity";
+window.OMS_AUTH_ENDPOINT = window.OMS_AUTH_ENDPOINT || "http://127.0.0.1:8787/api/feishu/identity";
 window.OMS_FEISHU_USER_WORKSPACE_MAP = window.OMS_FEISHU_USER_WORKSPACE_MAP || {};
