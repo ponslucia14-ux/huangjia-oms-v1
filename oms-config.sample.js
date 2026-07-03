8d2ef49e54e8fcb54e7c281bb5c8daa5b430cd9b
shilei <goodfriendshilei@163.com>
2026-07-04 02:18:33 +0800

    Add Feishu WebApp identity authentication

---
+++ oms-config.sample.js
@@ -0,0 +1,14 @@
+window.OMS_FEISHU_APP_ID = "cli_xxx";
+window.OMS_AUTH_ENDPOINT = "https://your-oms-auth.example.com/api/feishu/identity";
+window.OMS_FEISHU_USER_WORKSPACE_MAP = {
+  "user_june": "june",
+  "ou_june": "june",
+  "user_liujie": "liujie",
+  "ou_liujie": "liujie",
+  "user_sales": "huanhuan",
+  "ou_sales": "huanhuan",
+  "user_nana": "nana",
+  "ou_nana": "nana",
+  "user_boss": "boss",
+  "ou_boss": "boss"
+};
