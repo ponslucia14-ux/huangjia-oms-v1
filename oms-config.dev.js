31ebbd1a3407502301be2af5c57be4d93610aca2
shilei <goodfriendshilei@163.com>
2026-07-13 01:32:08 +0800

    Checkpoint EMP001 EMP008 workspace validation

--- oms-config.dev.js
+++ oms-config.dev.js
@@ -1,7 +1,7 @@
 window.OMS_ENV_CONFIGS = window.OMS_ENV_CONFIGS || {};
 window.OMS_ENV_CONFIGS.development = Object.freeze({
   configVersion: "oms.dev.20260710",
-  assetVersion: "emp008-workbench-v1-20260712-2",
+  assetVersion: "workspace-ia-v1-20260713",
   apiBaseUrl: "http://127.0.0.1:8787",
   feishuAppId: "cli_aaac7e6da2b95cfc",
   feishuRedirectUri: "http://127.0.0.1:8787/",
--- oms-config.prod.js
+++ oms-config.prod.js
@@ -1,7 +1,7 @@
 window.OMS_ENV_CONFIGS = window.OMS_ENV_CONFIGS || {};
 window.OMS_ENV_CONFIGS.production = Object.freeze({
   configVersion: "oms.prod.https.20260711",
-  assetVersion: "emp008-workbench-v1-20260712-2",
+  assetVersion: "workspace-ia-v1-20260713",
   apiBaseUrl: "https://api.wonderfulseki.cn",
   feishuAppId: "cli_aaac7e6da2b95cfc",
   feishuRedirectUri: "https://ponslucia14-ux.github.io/huangjia-oms-v1/",
